6e2af3f6aedcbddf51acaeebc9c5b2c40caa4fd6
Alexis <alexisingarao13@gmail.com>
2021-05-20 17:37:14 +0200

    Episode 3
    - Programming our character to fall with gravity and land on platforms! (using GDscript).
    - Jumping when a key (action) is pressed on the keyboard.
    - Adjusting the rate of falling and the amount of upward force used to jump.
    - Using 'constants' (like variables) in programming for: GRAVITY, JUMP FORCE, and SPEED.
    - Fixing the 'land-then-fall' issue by having move_and_side() return a value, updating our character's velocity
    - using a KinematicBody2D's is_on_floor() method, and the Vector2.UP constant

--- 2D Platformer Roguelite/Player.gd
+++ 2D Platformer Roguelite/Player.gd
@@ -1,13 +1,23 @@
+class_name Player
 extends KinematicBody2D
+
+export var gravity := 100
+export (float, 1, 10, 0.1) var player_speed := 3.8
+export (float, -20, -1, 0.1) var jump_force := -13.5
 
 var velocity := Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = player_speed * 100.0
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		velocity.x = -player_speed * 100.0
 	
-	move_and_slide(velocity)
+	velocity.y += gravity
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force * 100.0
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.5)
