9428d72feff60ba2f65ad2016f93b88299c0a8e4
Alexis <alexisingarao13@gmail.com>
2021-05-22 18:31:49 +0200

    Episode 9
    - How to switch between scenes! (change scenes) using one line of GDscript code
    - How to trigger your game to reset to the beginning of the level when your player falls below the game level
    - Adding and setting up Area2D objects / nodes (create a "fall zone" area)
    - Adding a signal to an object, that runs a gdscript function on another object (we connect the Area2D object's body_entered signal to a function on our character Steve's code.)

--- 2D Platformer Roguelite/Player.gd
+++ 2D Platformer Roguelite/Player.gd
@@ -1,22 +1,28 @@
 class_name Player
 extends KinematicBody2D
 
-export var gravity := 100
+export var gravity := 75
 export (float, 1, 10, 0.1) var player_speed := 3.8
-export (float, -20, -1, 0.1) var jump_force := -13.5
+export (float, -20, -1, 0.1) var jump_force := -19
 
 onready var player_sprite = $Sprite
+
+const GRAVITY_ON_FLOOR = 10
 
 var velocity := Vector2.ZERO
 var animated_sprites = ["idle", "happy"]
 var rand_idle_sprites
 
+
 func _ready() -> void:
 	randomize()
 	rand_idle_sprites = animated_sprites[randi() % 2]
 
+
 func _physics_process(_delta: float) -> void:
+	_gravity()
 	player_movement()
+
 
 func player_movement()-> void:
 	if Input.is_action_pressed("right"):
@@ -33,6 +39,8 @@ func player_movement()-> void:
 	if not is_on_floor():
 		player_sprite.play("air")
 	
+#	print(is_on_floor())
+	
 	velocity.y += gravity
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -41,3 +49,17 @@ func player_movement()-> void:
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.5)
+
+
+func _gravity():
+	if is_on_floor():   velocity.y = GRAVITY_ON_FLOOR
+	else:               velocity.y += gravity
+
+
+func _on_FallZone_body_entered(body: Node) -> void:
+	if body == self:
+		var _err = get_tree().change_scene("res://Level1.tscn")
+#		print(body.name, " ", body.get_children().size())
+#	if body == Enemy:
+#		print(body.name, " ", body.get_children().size())
+#		body.queue_free()
